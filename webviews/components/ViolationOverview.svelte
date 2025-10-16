7bc69cb7e9dd630c7cd0c9eb607a4ccfc5fd832b
Ruben Halman <no-reply@halmanandco.com>
2025-10-16 22:01:23 +0200

    Configure expressions (#9)
    * adds severity and fixes rule configuration
    * Bring back configure rules(with legacy support)

---
+++ webviews/components/ViolationOverview.svelte
@@ -0,0 +1,113 @@
+<script lang="ts">
+    import { onMount } from "svelte";
+    import NavigationBanner from "./Navigation.svelte";
+    import ViolationTableFull from "./ViolationTableFull.svelte";
+
+    let results;
+    let scanResults;
+    let allResults;
+    onMount(() => {
+        tsvscode.postMessage({ type: "init-view" });
+    });
+
+    let banner;
+
+    $: {
+        let details = [];
+        if (scanResults) {
+            for (let scanResult of scanResults) {
+                for (let ruleResult of scanResult.ruleResults) {
+                    let ruleDescription = ruleResult.ruleDefinition.description;
+                    let ruleLabel = ruleResult.ruleDefinition.label;
+                    let flowName = scanResult.flow.name;
+                    let severity = ruleResult.severity ?? "warning";
+                    let type;
+                    let metaType;
+                    let name;
+                    let dataType = "";
+                    let locationX = "";
+                    let locationY = "";
+                    let connectsTo = "";
+                    let expression = "";
+
+                    let initobj = {
+                        ruleDescription,
+                        ruleLabel,
+                        flowName,
+                        severity
+                    };
+                    if (ruleResult.occurs) {
+                        for (let detail of ruleResult.details) {
+                            name = detail.name ? detail.name : "";
+                            type = detail.type;
+                            metaType = detail.metaType;
+                            if (detail.details) {
+                                if (detail.details.dataType) {
+                                    dataType = detail.details.dataType;
+                                }
+                                if (detail.details.locationX) {
+                                    locationX = detail.details.locationX;
+                                }
+                                if (detail.details.locationY) {
+                                    locationY = detail.details.locationY;
+                                }
+                                if (detail.details.connectsTo) {
+                                    connectsTo =
+                                        detail.details.connectsTo.join();
+                                }
+                                if (detail.details.expression) {
+                                    expression = detail.details.expression;
+                                }
+                            }
+                            const detailObj = Object.assign(
+                                structuredClone(initobj),
+                                {
+                                    name,
+                                    type,
+                                    metaType,
+                                    dataType,
+                                    locationX,
+                                    locationY,
+                                    connectsTo,
+                                    expression,
+                                }
+                            );
+                            details.push(detailObj);
+                        }
+                    }
+                }
+            }
+        }
+        allResults = details;
+    }
+
+    function windowMessage(event) {
+        const message = event.data;
+        switch (message.type) {
+            case "init":
+                const state = tsvscode.getState();
+                if (state) {
+                    scanResults = state.value;
+                } else {
+                    scanResults = message.value;
+                }
+                return;
+            case "update":
+                scanResults = message.value;
+                tsvscode.setState({ scanResults });
+                return;
+        }
+    }
+</script>
+
+<svelte:window on:message={windowMessage} />
+<NavigationBanner
+    currentPage="viewAll"
+    showDownload
+    bind:this={banner}
+    on:navigate={(e) => banner.navigate(e, scanResults)}
+    on:download={() => results.download()}
+/>
+{#if allResults && allResults.length > 0}
+        <ViolationTableFull bind:this={results} bind:allResults />
+{/if}
---
+++ webviews/components/ViolationTableFull.svelte
@@ -0,0 +1,112 @@
+<script lang="typescript">
+    import { TabulatorFull as Tabulator } from "tabulator-tables";
+    import { onMount } from "svelte";
+
+    export let allResults;
+    let tableComponent;
+    let table;
+    let printData = [];
+
+    onMount(() => {
+        for(let result of allResults){
+            const detailObj = Object.assign({}, result);
+            printData.push(detailObj);
+        }
+        table = new Tabulator(tableComponent, {
+            data: allResults,
+            reactiveData: true,
+            layout: "fitColumns",
+            groupBy: ["ruleLabel"],
+            groupHeader: function (value, count, data, group) {
+                let description;
+                if (data && data.length > 0) {
+                    description = data[0].ruleDescription;
+                    return (
+                        value +
+                        "<span>(" +
+                        count +
+                        ")</span>" +
+                        "<p style='font-style: italic'>" +
+                        description +
+                        "</p>"
+                    );
+                } else {
+                    return (
+                        value +
+                        "<span style='color:#d00; margin-left:10px;'>(" +
+                        count +
+                        ")</span>"
+                    );
+                }
+            },
+            columns: [
+                { 
+                    title: "#", 
+                    formatter: "rownum", 
+                    width: 75
+                },
+                {
+                    title: "Name",
+                    field: "name",
+                    formatter: "textarea",
+                    minWidth: 150,
+                },
+                {
+                    title: "Severity",
+                    field: "severity",
+                    formatter: "textarea",
+                    minWidth: 150,
+                },
+                {
+                    title: "Type",
+                    field: "type",
+                    formatter: "plaintext",
+                    width: 150,
+                },
+                {
+                    title: "Flow name",
+                    field: "flowName",
+                    formatter: "textarea",
+                    minWidth: 150,
+                },
+                {
+                    title: "X coordinates",
+                    field: "locationX",
+                    width: 75,
+                },
+                {
+                    title: "Y coordinates",
+                    field: "locationY",
+                    width: 75,
+                },
+                {
+                    title: "Connects to",
+                    field: "connectsTo",
+                    formatter: "textarea",
+                    minWidth: 150,
+                },
+                {
+                    title: "Expression",
+                    field: "expression",
+                    formatter: "textarea",
+                    minWidth: 150,
+                },
+                {
+                    title: "DataType",
+                    field: "dataType",
+                    width: 150,
+                    formatter: "textarea"
+                }
+            ],
+        });
+    });
+
+    export function download() {
+        tsvscode.postMessage({
+            type: "download",
+            value: printData
+        });
+    }
+</script>
+
+<div bind:this={tableComponent} />
