7b7488e04fe138a69c1eeb2fe5803938aa0f7832
Ruben Halman <no-reply@halmanandco.com>
2025-11-29 07:45:41 +0100

    improve test

--- packages/cli/test/commands/flow/fix.test.ts
+++ packages/cli/test/commands/flow/fix.test.ts
@@ -1,4 +1,3 @@
-import { runCommand } from "@oclif/test";
 import { expect } from "chai";
 import { Config } from "@oclif/core";
 import sinon from "sinon";
@@ -8,76 +7,62 @@ import FlowFix from "../../../src/commands/flow/fix.js";
 import CoreFixService from "../../../src/libs/CoreFixService.js";
 
 describe("flow:fix", () => {
+
+  // e2e test using stub
   it("runs flow:fix e2e", async () => {
     sinon
       .stub(CoreFixService.prototype, "fix")
       .resolves(["/path/to/resolved.flow-meta.xml"]);
+
     const config: Config = new Config({
       root: "/tmp",
       pjson: {
         name: "test-cli",
         version: "0.0.1",
-        oclif: {
-          bin: "test-cli",
-        },
+        oclif: { bin: "test-cli" },
       },
     });
     await config.load();
+
     const scanResult: ScanResult = await new FlowFix(
       ["-r", "UnusedVariable", "-d", "."],
-      config,
+      config
     ).run();
+
     expect(scanResult.summary.message).to.contain(
-      "Fixed /path/to/resolved.flow-meta.xml",
+      "Fixed /path/to/resolved.flow-meta.xml"
     );
   });
 
-  describe("runs flow:fix flags", () => {
-    it("should throw an exception when rule is not specified", async () => {
-      const { stderr } = await runCommand<ScanResult>("flow:fix -d a");
-      expect(stderr).to.contain("Missing required flag rules");
+  // direct class call to check mutually exclusive flags
+  it("throws for mutually exclusive flags (direct class call)", async () => {
+    const config: Config = new Config({
+      root: "/tmp",
+      pjson: {
+        name: "test-cli",
+        version: "0.0.1",
+        oclif: { bin: "test-cli" },
+      },
     });
-    it("should throw an exception when mutually exclusive flags specified", async () => {
-      const { stderr } = await runCommand<ScanResult>(
-        "flow:fix -r UnusedVariable -d a -f ./test/commands/flow/fix.test.ts",
-      );
-      expect(stderr).to.contain("cannot also be provided when using");
-      expect(stderr).to.contain("--file");
-      expect(stderr).to.contain("--dir");
-    });
+    await config.load();
+
+    try {
+      await new FlowFix(
+        [
+          "-r", "UnusedVariable",
+          "-d", ".", "./src",
+          "-f", "./test/commands/flow/fix.test.ts",
+        ],
+        config
+      ).run();
+    } catch (e: any) {
+      expect(e.message).to.contain("cannot also be provided when using");
+      expect(e.message).to.contain("--file");
+      expect(e.message).to.contain("--dir");
+      return;
+    }
+
+    throw new Error("Expected error not thrown");
   });
 
-  describe("runs flow:fix unit", () => {
-    it("should throw an exception for mutually exclusive required", async () => {
-      const config: Config = new Config({
-        root: "/tmp",
-        pjson: {
-          name: "test-cli",
-          version: "0.0.1",
-          oclif: {
-            bin: "test-cli",
-          },
-        },
-      });
-      await config.load();
-      try {
-        await new FlowFix(
-          [
-            "-r",
-            "UnusedVariable",
-            "-d",
-            ".",
-            "./src",
-            "-f",
-            "./test/commands/flow/fix.test.ts",
-          ],
-          config,
-        ).run();
-      } catch (error: any) {
-        expect(error.message).to.contain("cannot also be provided when using");
-        expect(error.message).to.contain("--file");
-        expect(error.message).to.contain("--dir");
-      }
-    });
-  });
 });
